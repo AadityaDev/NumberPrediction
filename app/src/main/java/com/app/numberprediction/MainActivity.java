b914635bb076908b4dbdcbf42e857b424a295b65
AadityaDev <adityaverma.dev@gmail.com>
2018-06-10 00:50:36 +0530

    constant added

--- app/src/main/java/com/app/numberprediction/MainActivity.java
+++ app/src/main/java/com/app/numberprediction/MainActivity.java
@@ -1,15 +1,25 @@
 package com.app.numberprediction;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.app.numberprediction.constant.AppConstant;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TableLayout stk;
+    private BottomNavigationView navigation;
+    //head
+    private TableRow header;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +28,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -36,9 +43,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        stk = (TableLayout) findViewById(R.id.table_main);
+
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        init();
+    }
+
+    public void setTextViewProperty(TextView textView, String text){
+        textView.setText(text);
+        textView.setTextColor(Color.WHITE);
+        textView.setGravity(Gravity.CENTER);
+
+    }
+
+    public void setEditTextViewProperty(EditText editText, String text){
+        editText.setText(text);
+        editText.setTextColor(Color.WHITE);
+        editText.setGravity(Gravity.CENTER);
+    }
+    public void initalizeHeader(){
+        header = new TableRow(this);
+        TextView tv0 = new TextView(this);
+        setTextViewProperty(tv0,AppConstant.INDEX);
+        header.addView(tv0);
+        TextView tv1 = new TextView(this);
+        setTextViewProperty(tv1,AppConstant.S);
+        header.addView(tv1);
+        TextView tv2 = new TextView(this);
+        setTextViewProperty(tv2,AppConstant.D);
+        header.addView(tv2);
+        TextView tv3 = new TextView(this);
+        setTextViewProperty(tv3,AppConstant.A);
+        header.addView(tv3);
+        stk.addView(header);
+    }
+
+    public void init() {
+        initalizeHeader();
+        for (int i = 0; i < 25; i++) {
+            TableRow tbrow = new TableRow(this);
+            TextView t1v = new TextView(this);
+            setTextViewProperty(t1v,"" + i);
+            tbrow.addView(t1v);
+            EditText t2v = new EditText(this);
+            setEditTextViewProperty(t2v," " + i);
+            tbrow.addView(t2v);
+            EditText t3v = new EditText(this);
+            setEditTextViewProperty(t3v,"Rs." + i);
+            tbrow.addView(t3v);
+            EditText t4v = new EditText(this);
+            setEditTextViewProperty(t4v,"" + i * 15 / 32 * 10);
+            tbrow.addView(t4v);
+            stk.addView(tbrow);
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/app/numberprediction/constant/AppConstant.java
@@ -0,0 +1,24 @@
+package com.app.numberprediction.constant;
+
+public class AppConstant {
+
+    public static final String A = "A";
+    public static final String B = "B";
+    public static final String C = "C";
+    public static final String ATAB = " TAB A ";
+    public static final String BTAB = " TAB B ";
+    public static final String CTAB = " TAB C ";
+    public static final String ATABCOMPLEMENT = " TAB A' ";
+    public static final String BTABCOMPLEMENT = " TAB B' ";
+    public static final String CTABCOMPLEMENT = " TAB C' ";
+    public static final String ATABRESULT = " TAB A Result ";
+    public static final String BTABRESULT = " TAB B Result ";
+    public static final String CTABRESULT = " TAB C Result ";
+    public static final String SPACE = " ";
+    public static final String INDEX = "Index";
+    public static final String S = "S";
+    public static final String D = "D";
+    public static final String[] NUMS_INDEX = { "0" , "1", "2" , "3", "4" , "5", "6" , "7", "8", "9" };
+    public static final String[] INTABS_INDEX = { "11" , "21", "22" , "23", "24" , "25", "31" , "34", "41", "51", "61", "71" };
+
+}
